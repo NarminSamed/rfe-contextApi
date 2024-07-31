d232259bd5f11845cc418b69540f2aad9afade1c
NarminSamed <samedova.77n@gmail.com>
2024-07-31 15:54:45 +0400

    error page fixed

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import Lottie from "lottie-react";
+import animationData from "../../assets/animation/Animation - 1722413193309.json";
 
 const Error = () => {
   return (
-    <div>
-    <h1>Oops! Something went wrong.</h1>
-    <p>Sorry, the page you are looking for does not exist.</p>
-  </div>
-  )
-}
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <Lottie
+        animationData={animationData}
+        loop={true}
+        style={{ width: 300, height: 300, margin: "auto" }}
+      />
+      <h1>Oops! Something went wrong.</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
